72c79f69a44ec6cde55595fc876e6d89fd43bbf1
Gao Xiang <gaoxiangnumber1@126.com>
2016-11-01 21:46:01 +0800

    feat: add poller_ and active_channel_ data member to implement Reactor.

--- src/event_loop.cc
+++ src/event_loop.cc
@@ -1,28 +1,43 @@
-#include "event_loop.h"
+#include <event_loop.h>
 
-__thread EventLoop *g_loop_in_this_thread = nullptr;
+#include <assert.h> // assert()
+#include <poll.h> // poll()
 
-EventLoop::EventLoop(): looping_(false), thread_id_(CurrentThread::tid())
+#include <logging.h>
+#include <thread.h>
+
+using netlib::Thread;
+using netlib::EventLoop;
+
+// Every thread has its own instance of __thread variable.
+__thread EventLoop *t_loop_in_this_thread = nullptr;
+
+EventLoop::EventLoop():
+	looping_(false),
+	quit_(false),
+	thread_id_(Thread::ThreadId()),
+	poller_(new Poller(this))
 {
-	LOG_TRACE << "EventLoop created " << this << " in thread " << thread_id_;
-	// One loop per thread means that every thread can only have one EventLoop object,
-	// check it by following test. If this thread already has other EventLoop object,
-	// abort this thread.
-	if(g_loop_in_this_thread != nullptr)
+	LOG_INFO("EventLoop created %p in thread %d", this, thread_id_);
+	// One loop per thread means that every thread can have only one EventLoop object.
+	// If this thread already has another EventLoop object, abort this thread.
+	if(t_loop_in_this_thread != nullptr)
 	{
-		LOG_FATAL << "Another EventLoop " << g_loop_in_this_thread <<
-		          "exists in this thread " << thread_id_;
+		LOG_FATAL("Another EventLoop %p exists in this thread %d",
+		          t_loop_in_this_thread, thread_id_);
 	}
 	else
 	{
-		g_loop_in_this_thread = this;
+		// The thread that creates EventLoop object is the IO thread,
+		// whose main function is running EventLoop::Loop().
+		t_loop_in_this_thread = this;
 	}
 }
 
 EventLoop::~EventLoop()
 {
 	assert(looping_ == false);
-	g_loop_in_this_thread = nullptr;
+	t_loop_in_this_thread = nullptr;
 }
 
 void EventLoop::Loop()
@@ -30,23 +45,47 @@ void EventLoop::Loop()
 	assert(looping_ == false); // Not in looping.
 	AssertInLoopThread();
 	looping_ = true;
+	quit_ = false;
 
-	::poll(NULL, 0, 5 * 1000);
+	while(quit_ == false)
+	{
+		active_channel_.clear();
+		poller_->Poll(kPollTimeout, &active_channel_);
+		for(ChannelVector::iterator it = active_channel_.begin();
+		        it != active_channel_.end(); ++it)
+		{
+			(*it)->HandleEvent();
+		}
+	}
 
-	LOG_TRACE << "EventLoop " << this << " Stop looping.";
+	LOG_INFO("EventLoop %p Stop looping.", this);
 	looping_ = false;
 }
 
-// Return the object that hold by current thread, nullptr means current thread
-// is not the IO thread.
-EventLoop *GetEventLoopOfCurrentThread() // static member function.
+void AssertInLoopThread()
 {
-	return g_loop_in_this_thread;
+	if(thread_id_ != Thread::ThreadId()) // Must run Loop in IO thread.
+	{
+		LOG_FATAL("EventLoop %p was created in tid = %d, current tid = %d",
+		          this, thread_id_, Thread::ThreadId());
+	}
 }
 
-void EventLoop::AbortNotInLoopThread()
+void Quit()
 {
-	LOG_FATAL << "EventLoop::AbortNotInLoopThread - EventLoop " << this
-	          << " was created in thread_id = " << thread_id_
-	          << ", current thread id = " <<  CurrentThread::tid();
+	quit_ = true;
+}
+
+void UpdateChannel(Channel *channel)
+{
+	assert(channel->loop() == this);
+	AssertInLoopThread();
+	poller_->UpdateChannel(channel);
+}
+
+// Return the object that hold by current thread.
+// nullptr means current thread is not the IO thread.
+EventLoop *GetEventLoopOfCurrentThread() // static member function.
+{
+	return t_loop_in_this_thread;
 }
--- src/event_loop.h
+++ src/event_loop.h
@@ -1,40 +1,40 @@
-#ifndef NETLIB_SRC_NET_EVENTLOOP_H_
-#define NETLIB_SRC_NET_EVENTLOOP_H_
+#ifndef NETLIB_SRC_EVENTLOOP_H_
+#define NETLIB_SRC_EVENTLOOP_H_
+
+#include <sys/types.h> // pid_t
 
 #include <non_copyable.h>
 
 namespace netlib
 {
-namespace net
-{
+
+class Channel;
+class Poller;
+
 class EventLoop: public netlib::NonCopyable
 {
 public:
-	EventLoop();
+	EventLoop(); // Check whether satisfy `one loop per thread`.
 	~EventLoop();
 
 	void Loop();
-	bool IsInLoopThread() const
-	{
-		return thread_id_ == CurrentThread::tid();
-	}
-	void AssertInLoopThread()
-	{
-		if(!IsInLoopThread())
-		{
-			AbortNotInLoopThread();
-		}
-	}
+	void AssertInLoopThread();
+	void Quit();
+
+	void UpdateChannel(Channel *channel);
 
 	// Return the object that hold by current thread.
 	static EventLoop *GetEventLoopOfCurrentThread();
 
 private:
-	void AbortNotInLoopThread();
+	using ChannelVector = std::vector<Channel*>;
 
 	bool looping_; // Atomic
+	bool quit_; // Atomic
 	const pid_t thread_id_;
+	std::unique_ptr<Poller> poller_;
+	ChannelVector active_channel_;
 };
+
 }
-}
-#endif // NETLIB_SRC_NET_EVENTLOOP_H_
+#endif // NETLIB_SRC_EVENTLOOP_H_
