1f127b9508e5935edd6a28bd00d263510ae1face
Gao Xiang <gaoxiangnumber1@126.com>
2016-11-03 23:02:25 +0800

    feat: add timer_queue for EventLoop class

--- src/event_loop.cc
+++ src/event_loop.cc
@@ -3,20 +3,29 @@
 #include <assert.h> // assert()
 #include <poll.h> // poll()
 
+#include <channel.h>
 #include <logging.h>
+#include <poller.h>
 #include <thread.h>
+#include <timer_queue.h>
 
-using netlib::Thread;
 using netlib::EventLoop;
+using netlib::Thread;
+using netlib::TimerId;
+using netlib::TimerCallback;
+using netlib::TimerQueue;
+using netlib::TimeStamp;
 
 // Every thread has its own instance of __thread variable.
 __thread EventLoop *t_loop_in_this_thread = nullptr;
+const int kPollTimeout = 10 * 1000;
 
 EventLoop::EventLoop():
 	looping_(false),
 	quit_(false),
 	thread_id_(Thread::ThreadId()),
-	poller_(new Poller(this))
+	poller_(new Poller(this)), // Set poller_'s owner_loop_ to this EventLoop.
+	timer_queue_(new TimerQueue(this))
 {
 	LOG_INFO("EventLoop created %p in thread %d", this, thread_id_);
 	// One loop per thread means that every thread can have only one EventLoop object.
@@ -40,17 +49,48 @@ EventLoop::~EventLoop()
 	t_loop_in_this_thread = nullptr;
 }
 
+// Return the object that hold by current thread.
+// nullptr means current thread is not the IO thread.
+EventLoop *EventLoop::GetEventLoopOfCurrentThread() // static member function.
+{
+	return t_loop_in_this_thread;
+}
+
+// We can set quit_ to be true by calling set_quit(true) to terminate loop. But the actual
+// quit happen when EventLoop::Loop() check `while(quit_ == false)`. If set_quit(true)
+// happens in other threads(not in current IO thread), the delay can be a few seconds.
+// set_quit(true) only set a flag, not generate an interrupt or a signal. So, when Loop
+// is blocking in some calls, set_quit(true) can't take effects until Loop is unblocking.
+void EventLoop::set_quit(bool quit)
+{
+	quit_ = quit;
+}
+
+void EventLoop::AssertInLoopThread()
+{
+	if(thread_id_ != Thread::ThreadId()) // Must run Loop in IO thread.
+	{
+		LOG_FATAL("EventLoop %p was created in tid = %d, current tid = %d",
+		          this, thread_id_, Thread::ThreadId());
+	}
+}
+
 void EventLoop::Loop()
 {
+	// Check invariants by assert().
 	assert(looping_ == false); // Not in looping.
-	AssertInLoopThread();
+	AssertInLoopThread(); // Must run Loop in IO thread.
+
 	looping_ = true;
 	quit_ = false;
-
+	// Loop forever unless quit_ is set to `true` by current IO thread or other thread.
 	while(quit_ == false)
 	{
-		active_channel_.clear();
+		active_channel_.clear(); // Clear old active channel vector.
+		// Invoke ::poll() to get the number of active IO events and
+		// invoke FillActiveChannel to fill active_channel_.
 		poller_->Poll(kPollTimeout, &active_channel_);
+		// Now, each channel in active_channel_ has IO events happened.
 		for(ChannelVector::iterator it = active_channel_.begin();
 		        it != active_channel_.end(); ++it)
 		{
@@ -62,30 +102,33 @@ void EventLoop::Loop()
 	looping_ = false;
 }
 
-void AssertInLoopThread()
+// Runs callback at `time_stamp`.
+TimerId EventLoop::RunAt(const TimerCallback &callback,const TimeStamp &time_stamp)
 {
-	if(thread_id_ != Thread::ThreadId()) // Must run Loop in IO thread.
-	{
-		LOG_FATAL("EventLoop %p was created in tid = %d, current tid = %d",
-		          this, thread_id_, Thread::ThreadId());
-	}
+	// TimerId AddTimer(const TimerCallback &callback,
+	//                  TimeStamp expired_time,
+	//                  double interval);
+	return timer_queue_->AddTimer(callback, time_stamp, 0.0);
 }
 
-void Quit()
+// Run callback after `delay` seconds.
+TimerId EventLoop::RunAfter(const TimerCallback &callback, double delay)
 {
-	quit_ = true;
+	TimeStamp time_stamp(AddTime(TimeStamp::Now(), delay));
+	return RunAt(callback, time_stamp);
 }
 
-void UpdateChannel(Channel *channel)
+// Run callback every `interval` seconds.
+TimerId EventLoop::RunEvery(const TimerCallback &callback, double interval)
 {
-	assert(channel->loop() == this);
+	TimeStamp time_stamp(AddTime(TimeStamp::Now(), interval));
+	return timer_queue_->AddTimer(callback, time_stamp, interval);
+}
+
+void EventLoop::UpdateChannel(Channel *channel)
+{
+	// Only can update channel that this EventLoop owns.
+	assert(channel->owner_loop() == this);
 	AssertInLoopThread();
 	poller_->UpdateChannel(channel);
-}
-
-// Return the object that hold by current thread.
-// nullptr means current thread is not the IO thread.
-EventLoop *GetEventLoopOfCurrentThread() // static member function.
-{
-	return t_loop_in_this_thread;
 }
--- src/event_loop.h
+++ src/event_loop.h
@@ -3,37 +3,60 @@
 
 #include <sys/types.h> // pid_t
 
+#include <memory> // unique_ptr<>
+#include <vector> // vector<>
+
+#include <callback.h>
 #include <non_copyable.h>
+#include <time_stamp.h>
+#include <timer_id.h>
 
 namespace netlib
 {
 
 class Channel;
 class Poller;
+class TimerQueue;
 
-class EventLoop: public netlib::NonCopyable
+class EventLoop: public NonCopyable
 {
 public:
 	EventLoop(); // Check whether satisfy `one loop per thread`.
-	~EventLoop();
+	~EventLoop(); // Force out-line dtor, for unique_ptr members.
 
-	void Loop();
-	void AssertInLoopThread();
-	void Quit();
-
-	void UpdateChannel(Channel *channel);
-
-	// Return the object that hold by current thread.
+	// Getter. Return the object that hold by current thread.
 	static EventLoop *GetEventLoopOfCurrentThread();
+	TimeStamp poll_return_time() const
+	{
+		return poll_return_time_;
+	}
+	// Setter.
+	void set_quit(bool quit);
+
+	void AssertInLoopThread();
+	// Loop forever. Must be called in the same thread as creation of the object.
+	void Loop();
+	// Runs callback at `time_stamp`.
+	TimerId RunAt(const TimerCallback &callback, const TimeStamp &time_stamp);
+	// Run callback after `delay` seconds.
+	TimerId RunAfter(const TimerCallback &callback, double delay);
+	// Run callback every `interval` seconds.
+	TimerId RunEvery(const TimerCallback &callback, double interval);
+	// Internal use only
+	void UpdateChannel(Channel *channel);
 
 private:
 	using ChannelVector = std::vector<Channel*>;
 
-	bool looping_; // Atomic
-	bool quit_; // Atomic
+	bool looping_;
+	bool quit_;
 	const pid_t thread_id_;
-	std::unique_ptr<Poller> poller_;
+	// Only one unique_ptr at a time can point to a given object. The object to which a
+	// unique_ptr points is destroyed when the unique_ptr is destroyed.
+	TimeStamp poll_return_time_; // Time when poll returns, usually means data arrival.
 	ChannelVector active_channel_;
+	std::unique_ptr<Poller> poller_;
+	std::unique_ptr<TimerQueue> timer_queue_;
 };
 
 }
