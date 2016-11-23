10a34b1e44493656016fb87e2f2f74ed528c97ea
Gao Xiang <gaoxiangnumber1@126.com>
2016-11-23 22:10:59 +0800

    feat: add FindCRLF() for Buffer class

--- netlib/buffer.h
+++ netlib/buffer.h
@@ -7,7 +7,6 @@
 
 #include <vector>
 #include <string>
-#include <algorithm>
 
 namespace netlib
 {
@@ -18,14 +17,14 @@ public:
 	static const int kPrepend = 8;
 	static const int kInitialSize = 1024;
 
-	Buffer():
-		buffer_(kPrepend + kInitialSize),
+	explicit Buffer(int initial_size = kInitialSize):
+		buffer_(kPrepend + initial_size),
 		read_index_(kPrepend),
 		write_index_(kPrepend)
 	{
 		assert(PrependableByte() == kPrepend);
 		assert(ReadableByte() == 0);
-		assert(WritableByte() == kInitialSize);
+		assert(WritableByte() == initial_size);
 	}
 	// Default dtor/copy-ctor/assignment-op are okay.
 
@@ -41,20 +40,29 @@ public:
 	{
 		return static_cast<int>(buffer_.size()) - write_index_;
 	}
+
+	const char *FindCRLF(const char *start = BeginRead()) const
+	{
+		assert(BeginRead() <= start && start <= BeginWrite());
+		const char *ret = nullptr, *end = BeginWrite() - 1;
+		for(const char *ptr = start; ptr < end; ++ptr)
+		{
+			if(*ptr == '\r' && *(ptr + 1) == '\n')
+			{
+				ret = ptr;
+				break;
+			}
+		}
+		return ret;
+	}
+
 	void Append(const char *data, int length)
 	{
 		EnsureWritableByte(length);
-		std::copy(data, data + length, BeginWrite());
-		FinishWritten(length);
+		Copy(data, length, BeginWrite());
+		write_index_ += length;
 	}
-	void EnsureWritableByte(int length)
-	{
-		if(WritableByte() < length)
-		{
-			MakeSpace(length);
-		}
-		assert(WritableByte() >= length);
-	}
+	// TODO: Is necessary to overload?
 	const char *BeginWrite() const
 	{
 		return Begin() + write_index_;
@@ -63,16 +71,12 @@ public:
 	{
 		return Begin() + write_index_;
 	}
-	void FinishWritten(int length)
-	{
-		write_index_ += length;
-	}
 
 	void RetrieveAll()
 	{
 		read_index_ = write_index_ = kPrepend;
 	}
-	const char *Peek() const // The begin pointer of data to be read.
+	const char *BeginRead() const // The begin pointer of data to be read.
 	{
 		return Begin() + read_index_;
 	}
@@ -87,7 +91,7 @@ public:
 		RetrieveAll();
 		return str;
 	}
-	int ReadFd(int fd, int *saved_errno);
+	int ReadFd(int fd, int &saved_errno);
 
 private:
 	const char *Begin() const
@@ -98,21 +102,33 @@ private:
 	{
 		return &(*buffer_.begin());
 	}
-	void MakeSpace(int length)
+	void EnsureWritableByte(int length)
 	{
-		// 1. There is really not enough space.
-		if(PrependableByte() + WritableByte() < length + kPrepend)
+		if(WritableByte() < length)
 		{
-			buffer_.resize(write_index_ + length);
+			// 1. There is really not enough space.
+			if(PrependableByte() + WritableByte() < length + kPrepend)
+			{
+				buffer_.resize(write_index_ + length);
+			}
+			// 2. Move readable data to the front, make space inside buffer.
+			else
+			{
+				assert(read_index_ > kPrepend);
+				int readable_byte = ReadableByte();
+				Copy(BeginRead(), readable_byte, Begin() + kPrepend);
+				read_index_ = kPrepend;
+				write_index_ = read_index_ + readable_byte;
+				assert(readable_byte == ReadableByte());
+			}
 		}
-		// 2. Move readable data to the front, make space inside buffer.
-		else
+		assert(WritableByte() >= length);
+	}
+	void Copy(const char *to_copy, const int length, char *to_write)
+	{
+		for(int index = 0; index < length; ++index)
 		{
-			int readable = ReadableByte();
-			std::copy(Begin() + read_index_, Begin() + write_index_, Begin() + kPrepend);
-			read_index_ = kPrepend;
-			write_index_ = read_index_ + readable;
-			assert(readable == ReadableByte());
+			*(to_write++) = *(data + index);
 		}
 	}
 
