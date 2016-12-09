c1b05edb4b495c992c956f5380d02e5f68a93b41
Gao Xiang <gaoxiangnumber1@126.com>
2016-12-09 23:06:26 +0800

    feat: finish TcpConnection class Interface part

--- netlib/tcp_connection.h
+++ netlib/tcp_connection.h
@@ -1,13 +1,12 @@
 #ifndef NETLIB_NETLIB_TCP_CONNECTION_H_
 #define NETLIB_NETLIB_TCP_CONNECTION_H_
 
+#include <string>
+
+#include <netlib/buffer.h>
+#include <netlib/callback.h>
 #include <netlib/non_copyable.h>
 #include <netlib/socket_address.h>
-#include <netlib/callback.h>
-#include <netlib/buffer.h>
-
-#include <memory>
-#include <string>
 
 namespace netlib
 {
@@ -16,6 +15,17 @@ class EventLoop;
 class Socket;
 class Channel;
 
+// Interface:
+// Ctor.
+// Dtor.
+// SetTcpNoDelay.
+// ConnectEstablished -> -connection_callback_.
+// Send -> -SendInLoop. (SendInLoop -> write_complete_cb_, high_water_mark_cb_)
+// Shutdown -> -ShutdownInLoop.
+// ForceClose -> -ForceCloseInLoop. (ForceCloseInLoop -> -HandleClose.)
+// ConnectDestroyed -> -connection_callback_.
+
+// TCP connection, for both client and server usage.
 // Because of the ambiguous lifetime of TcpConnection, we use shared_ptr<> to
 // manage TcpConnection object by default.
 // One TcpConnection object represents "one TCP connection". Once the TCP
@@ -26,7 +36,6 @@ class TcpConnection: public NonCopyable,
 	public std::enable_shared_from_this<TcpConnection>
 {
 public:
-	// enum CallbackType {};
 	// Construct a TcpConnection with a connected socket.
 	// User should not create this object.
 	TcpConnection(EventLoop *event_loop,
@@ -35,6 +44,7 @@ public:
 	              const SocketAddress &local,
 	              const SocketAddress &peer);
 	~TcpConnection();
+
 	// Getter.
 	EventLoop *loop() const
 	{
@@ -52,10 +62,15 @@ public:
 	{
 		return peer_address_;
 	}
-	bool Connected() const
+	Buffer *input_buffer()
 	{
-		return state_ == CONNECTED;
+		return &input_buffer_;
 	}
+	Buffer *output_buffer()
+	{
+		return &output_buffer_;
+	}
+
 	// Setter.
 	void set_connection_callback(const ConnectionCallback &callback)
 	{
@@ -65,78 +80,98 @@ public:
 	{
 		message_callback_ = callback;
 	}
-	void set_write_complete_callback(const WriteCompleteCallback &callback)
-	{
-		write_complete_callback_ = callback;
-	}
 	void set_high_water_mark_callback(const HighWaterMarkCallback &callback,
 	                                  int high_water_mark)
 	{
 		high_water_mark_callback_ = callback;
 		high_water_mark_ = high_water_mark;
 	}
+	void set_write_complete_callback(const WriteCompleteCallback &callback)
+	{
+		write_complete_callback_ = callback;
+	}
 	// Internal use only.
 	void set_close_callback(const CloseCallback &callback)
 	{
 		close_callback_ = callback;
 	}
+
+	bool Connected() const
+	{
+		return state_ == CONNECTED;
+	}
+	bool Disconnected() const
+	{
+		return state_ == DISCONNECTED;
+	}
+	void SetTcpNoDelay(bool on);
+
 	// Called when TcpServer accept a new connection. Should be called only once.
 	void ConnectEstablished();
+	// Thread safe.
+	void Send(const std::string &message);
+	void Send(Buffer *message); // Swap data
+	// TODO: NOT thread safe, no simultaneous calling.
+	void Shutdown();
+	void ForceClose();
 	// Called when TcpServer remove this TcpConnection from its map.
 	// Should be called only once.
 	void ConnectDestroyed();
 
-	// Thread safe.
-	void Send(const std::string &message);
-	// Thread safe.
-	void Shutdown();
-	void SetTcpNoDelay(bool on);
-
 private:
-	enum State {CONNECTING, CONNECTED, DISCONNECTING, DISCONNECTED};
+	enum State
+	{
+		DISCONNECTED,
+		CONNECTING,
+		CONNECTED,
+		DISCONNECTING
+	};
 	void set_state(State state)
 	{
 		state_ = state;
 	}
+	const char * StateToCString() const;
+
 	// Channel's *_callback_ is Handle*() here.
 	void HandleRead(TimeStamp receive_time);
 	void HandleWrite();
 	void HandleClose();
 	void HandleError();
 
-	void SendInLoop(const std::string &message);
+	void SendInLoop(const char *data, int length);
 	void ShutdownInLoop();
+	void ForceCloseInLoop();
 
 	EventLoop *loop_;
-	// "listen_address.ToHostPort()" + "#next_connection_id_". Set by TcpServer.
-	std::string name_;
+	// "listen_address.ToIpPortString()" + "#next_connection_id_". Set by TcpServer.
+	const std::string name_;
 	State state_; // FIXME: Use atomic variable
-	// We don't expose following classes to client.
+	// We don't expose unique_ptr<T> classes to client.
 	// TcpConnection owns TCP socket, its destructor will `::close(socket_)` in the
-	// destructor of Socket object.
+	// destructor of the Socket object.
 	std::unique_ptr<Socket> socket_;
 	// TcpConnection gets the socket_'s IO events by channel_. It deals with the writable
 	// events by itself, and pass readable events to user by message_callback_.
 	std::unique_ptr<Channel> channel_;
-	SocketAddress local_address_;
-	SocketAddress peer_address_;
-	// std::function<void(const TcpConnectionPtr&)>;
+	const SocketAddress local_address_;
+	const SocketAddress peer_address_;
+	Buffer input_buffer_;
+	Buffer output_buffer_; // FIXME: Use list<Buffer> as output buffer.
 	// TcpServer::HandleNewConnection -> TcpConnection::ConnectEstablished() ->
 	// connection_callback_.
 	ConnectionCallback connection_callback_;
-	// std::function<void(const TcpConnectionPtr&, const char*, int)>
 	// Called in HandleRead(), that is, when the socket is readable(message arrives).
 	MessageCallback message_callback_;
+	int high_water_mark_;
+	static const int kInitialHighWaterMark = 64 * 1024 * 1024;
+	HighWaterMarkCallback high_water_mark_callback_;
+	WriteCompleteCallback write_complete_callback_;
 	// close_callback_ can be only used by TcpServer and TcpClient, to notify that
 	// they should remove this TcpConnection object's shared_ptr; not used by user,
-	// user still use connection_callback_. Called in HandleClose().
-	// Bind to TcpServer::RemoveConnection().
-	WriteCompleteCallback write_complete_callback_;
-	HighWaterMarkCallback high_water_mark_callback_;
+	// user only use connection_callback_.
+	// Called in HandleClose(). Bind to TcpServer::RemoveConnection().
 	CloseCallback close_callback_;
-	int high_water_mark_;
-	Buffer input_buffer_;
-	Buffer output_buffer_;
+	// May use later. boost::any context_;
 };
 
 }
