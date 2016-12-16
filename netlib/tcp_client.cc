cd1b2de59e775f4ba15b7e7db99f617796f0424f
Gao Xiang <gaoxiangnumber1@126.com>
2016-12-16 21:54:38 +0800

    feat: finish TcpClient class

---
+++ netlib/tcp_client.cc
@@ -0,0 +1,139 @@
+#include <netlib/tcp_client.h>
+#include <netlib/event_loop.h>
+#include <netlib/connector.h>
+#include <netlib/tcp_connection.h>
+#include <netlib/logging.h>
+#include <netlib/socket_address.h>
+#include <netlib/socket_operation.h>
+
+using std::bind;
+using std::string;
+using std::placeholders::_1;
+using netlib::TcpClient;
+
+TcpClient::TcpClient(EventLoop *event_loop,
+                     const SocketAddress &server_address,
+                     const string &name):
+	loop_(CHECK_NOT_NULL(event_loop)),
+	connector_(new Connector(loop_, server_address)),
+	name_(name),
+	retry_(false),
+	connect_(true),
+	next_connection_id_(1),
+	connection_callback_(DefaultConnectionCallback),
+	message_callback_(DefaultMessageCallback)
+{
+	connector_->set_new_connection_callback(
+	    bind(&TcpClient::HandleNewConnection, this, _1));
+	LOG_INFO("TcpClient::TcpClient[%s] - connector %p",
+	         name_.c_str(),
+	         connector_.get());
+}
+
+TcpClient::~TcpClient()
+{
+	LOG_INFO("TcpClient::~TcpClient[%s] - connector %p",
+	         name_.c_str(),
+	         connector_.get());
+	TcpConnectionPtr connection;
+	bool is_unique = false;
+	{
+		MutexLockGuard lock(mutex_);
+		is_unique = connection_.unique();
+		connection = connection_;
+	}
+	if(connection)
+	{
+		assert(loop_ == connection->loop());
+		// TODO: Already set_close_callback in HandleNewConnection(), why set it again?
+		// FIXME: not 100% safe, if we are in different thread
+		// CloseCallback cb = bind(&detail::removeConnection, loop_, _1);
+		// loop_->runInLoop(bind(&TcpConnection::setCloseCallback, connection, cb));
+		if(is_unique == true)
+		{
+			connection->ForceClose();
+		}
+	}
+	else
+	{
+		connector_->Stop();
+		// FIXME: hack.
+		// loop_->RunAfter(1, bind(&detail::removeConnector, connector_));
+	}
+}
+
+void TcpClient::Connect()
+{
+	// FIXME: check state
+	LOG_INFO("TcpClient::connect[%s] - connecting to %s",
+	         name_.c_str(),
+	         connector_->server_address().ToIpPortString().c_str());
+	connect_ = true;
+	connector_->Start();
+}
+void TcpClient::Disconnect()
+{
+	connect_ = false;
+	{
+		MutexLockGuard lock(mutex_);
+		if(connection_)
+		{
+			connection_->Shutdown();
+		}
+	}
+}
+void TcpClient::Stop()
+{
+	connect_ = false;
+	connector_->Stop();
+}
+
+void TcpClient::HandleNewConnection(int socket_fd)
+{
+	loop_->AssertInLoopThread();
+	SocketAddress peer_address(nso::GetPeerAddress(socket_fd));
+	char buffer[32];
+	snprintf(buffer, sizeof buffer, ":%s#%d",
+	         peer_address.ToIpPortString().c_str(),
+	         next_connection_id_++);
+	string connection_name = name_ + buffer;
+
+	SocketAddress local_address(nso::GetLocalAddress(socket_fd));
+	// FIXME poll with zero timeout to double confirm the new connection.
+	// FIXME use make_shared if necessary.
+	TcpConnectionPtr connection(new TcpConnection(loop_,
+	                            connection_name,
+	                            socket_fd,
+	                            local_address,
+	                            peer_address));
+
+	connection->set_connection_callback(connection_callback_);
+	connection->set_message_callback(message_callback_);
+	connection->set_write_complete_callback(write_complete_callback_);
+	connection->set_close_callback(
+	    bind(&TcpClient::RemoveConnection, this, _1)); // FIXME: unsafe
+	{
+		MutexLockGuard lock(mutex_);
+		connection_ = connection;
+	}
+	connection->ConnectEstablished();
+}
+
+void TcpClient::RemoveConnection(const TcpConnectionPtr& connection)
+{
+	loop_->AssertInLoopThread();
+	assert(loop_ == connection->loop());
+	{
+		MutexLockGuard lock(mutex_);
+		assert(connection_ == connection);
+		connection_.reset();
+	}
+	loop_->QueueInLoop(bind(&TcpConnection::ConnectDestroyed, connection));
+	if (retry_ == true && connect_ == true)
+	{
+		LOG_INFO("TcpClient::connect[%s] - Reconnecting to %s",
+		         name_.c_str(),
+		         connector_->server_address().ToIpPortString().c_str());
+		connector_->Restart();
+	}
+}
