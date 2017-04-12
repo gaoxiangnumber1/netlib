cd7cd392a1621f901278e58d6bd1c23421977f60
Gao Xiang <gaoxiangnumber1@126.com>
2017-04-12 22:03:31 +0800

    feat: add LongestCommonSubseqence

--- alg/dp.cc
+++ alg/dp.cc
@@ -46,10 +46,72 @@ void TestMaxSubArraySum()
 	printf("All case pass.\n");
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void LongestCommonSubsequence() {}
+void LongestCommonSubsequence(int *s1, int s1_first, int s1_last,
+                              int *s2, int s2_first, int s2_last)
+{
+	int s1_length = s1_last - s1_first, s2_length = s2_last - s2_first;
+	int length[s1_length + 1][s2_length + 1];
+	for(int row = 0; row <= s1_length; ++row)
+	{
+		length[row][0] = 0;
+	}
+	for(int col = 0; col <= s2_length; ++col)
+	{
+		length[0][col] = 0;
+	}
+	for(int row = 1; row <= s1_length; ++row)
+	{
+		for(int col = 1; col <= s2_length; ++col)
+		{
+			if(s1[s1_first + row - 1] == s2[s2_first + col - 1])
+			{
+				length[row][col] = length[row - 1][col - 1] + 1;
+			}
+			else
+			{
+				length[row][col] = max(length[row - 1][col], length[row][col - 1]);
+			}
+		}
+	}
+
+	// Print LCS
+	int max_length = length[s1_length][s2_length];
+	printf("LCS_length = %d: ", max_length);
+	int answer[max_length], index = max_length, row = s1_length, col = s2_length;
+	while(row >= 1 && col >= 1)
+	{
+		if(s1[row - 1] == s2[col - 1])
+		{
+			answer[--index] = s1[row - 1];
+			--row;
+			--col;
+		}
+		else if(length[row - 1][col] >= length[row][col - 1])
+		{
+			--row;
+		}
+		else
+		{
+			--col;
+		}
+	}
+	for(index = 0; index < max_length; ++index)
+	{
+		printf("%d%c", answer[index], (index == max_length - 1 ? '\n' : ' '));
+	}
+}
+void TestLongestCommonSubsequence()
+{
+	printf("----------TestMaxSubArraySum----------\n");
+	int s1[7] = {1,2,3,2,4,1,2};
+	int s2[6] = {2,4,3,1,2,1};
+	LongestCommonSubsequence(s1, 0, 7, s2, 0, 6); // LCS_length = 4: 2 3 2 1
+	printf("All case pass.\n");
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
 	TestMaxSubArraySum();
+	TestLongestCommonSubsequence();
 }
